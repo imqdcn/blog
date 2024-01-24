573a2661888f845ad7a472c011c5f2c03c141bd5
ycluo <imqdcn@gmail.com>
2024-01-24 11:58:50 +0800

    [feat] 更新AI文章

--- public/sidebar/AI.js
+++ public/sidebar/AI.js
@@ -3,7 +3,9 @@ export default [
         text: 'AI',
         items: [
           { text: 'ChatGPT使用指南', link: '/chatGPT/01.ChatGPT使用指南' },
-          { text: 'new bing注册指南', link: '/chatGPT/02.new bing注册指南' }
+          { text: 'new bing注册指南', link: '/chatGPT/02.new-bing注册指南' },
+          { text: 'ChatGPT-进阶技巧', link: '/chatGPT/03.ChatGPT-进阶技巧' },
+          { text: 'Google Bard使用初体验与ChatGPT的比较', link: '/chatGPT/04.Google-Bard使用初体验与ChatGPT的比较' }
         ]
       },
   ]
